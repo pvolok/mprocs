a68464d4199f9913eb7e2f14a4605a7f9d56dc57
Pavel Volokitin <pavelvolokitin@gmail.com>
2021-09-09 18:40:01 +0200

    Move layout and procs components

--- src/main.ml
+++ src/main.ml
@@ -6,53 +6,6 @@ open Lwd_infix
 
 let help _ = I.string "<C-a: Output>" |> Ui.atom |> Lwd.return
 
-module Layout = struct
-  type constr =
-    | Fixed of int
-    | Fill
-
-  let calc_layout len parts =
-    let for_fixed, fill_count =
-      List.fold_left parts ~init:(0, 0)
-        ~f:(fun (for_fixed, fill_count) (constr, _) ->
-          match constr with
-          | Fixed n -> (for_fixed + n, fill_count)
-          | Fill -> (for_fixed, fill_count + 1))
-    in
-    let per_fill = (len - for_fixed) / fill_count in
-    let _, acc =
-      List.fold_left parts ~init:(len, []) ~f:(fun (len, acc) (constr, ui) ->
-          let part_len = match constr with Fixed n -> n | Fill -> per_fill in
-          let part_len = min len part_len in
-          (len - part_len, (part_len, ui) :: acc))
-    in
-    List.rev acc
-
-  let hor (wsize : (int * int) Lwd.t) parts =
-    let wsizes =
-      let$ w, h = wsize in
-      calc_layout w parts |> Array.of_list
-      |> Array.map ~f:(fun (l, _) -> (l, h))
-    in
-    let parts =
-      List.mapi parts ~f:(fun i (_, part) ->
-          part (Lwd.map wsizes ~f:(fun sizes -> sizes.(i))))
-    in
-    Lwd_utils.pack Ui.pack_x parts
-
-  let vert (wsize : (int * int) Lwd.t) parts =
-    let wsizes =
-      let$ w, h = wsize in
-      calc_layout h parts |> Array.of_list
-      |> Array.map ~f:(fun (l, _) -> (w, l))
-    in
-    let parts =
-      List.mapi parts ~f:(fun i (_, part) ->
-          part (Lwd.map wsizes ~f:(fun sizes -> sizes.(i))))
-    in
-    Lwd_utils.pack Ui.pack_y parts
-end
-
 let procs (cols, rows) =
   I.tabulate cols rows (fun col row ->
       let uchar =
@@ -85,7 +38,7 @@ let vwinsize =
 (* Let's make use of the fancy let-operators recently added to OCaml *)
 open Lwd_infix
 let ui procs_ui term_ui =
-  Layout.(
+  Ui_layout.(
     vert (Lwd.get vwinsize)
       [
         ( Fill,
@@ -121,7 +74,7 @@ let run () =
               Vterm.setSize ~size:{ rows = h; cols = w } pt.vterm))
   in
 
-  let procs_ui = W_procs.make in
+  let procs_ui = Ui_procs.make in
   let term_ui = Ui_term.make ~on_resize in
   let ui =
     Lwd.map (ui procs_ui term_ui)
