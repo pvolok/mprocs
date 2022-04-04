030fe0cfa705012693e6e04a7f8044c2a3ce909d
Pavel Volokitin <pavelvolokitin@gmail.com>
2022-04-04 20:30:11 +0200

    Windows release build fixes

--- release.cmd
+++ release.cmd
@@ -1,12 +1,14 @@
 SET VERSION=0.1.0
 
-RMDIR \Q \S release
-MD release
+RMDIR /Q /S release || exit /b
+MKDIR release\mprocs-%VERSION%-win64 || exit /b
 
 :: Windows 64
 
-cargo build --release
+cargo build --release || exit /b
 
-COPY target\release\mprocs.exe release\mprocs-%VERSION%-win64.exe
+COPY target\release\mprocs.exe release\mprocs-%VERSION%-win64\mprocs.exe || exit /b
 
-upx --brute release\mprocs-%VERSION%-win64.exe
+:: upx --brute release\mprocs-%VERSION%-win64\mprocs.exe || exit /b
+
+tar.exe -a -c -f release\mprocs-%VERSION%-win64.zip -C release\mprocs-%VERSION%-win64 mprocs.exe
