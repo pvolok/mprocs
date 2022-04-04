15bafe8a4d1ef28c9c5450be5ff0656e5e479a54
Pavel Volokitin <pavelvolokitin@gmail.com>
2022-04-04 20:28:58 +0200

    Archive release on unix

--- release.sh
+++ release.sh
@@ -3,24 +3,35 @@ set -e
 VERSION=0.1.0
 
 rm -rf release
-mkdir -p release
 
 # Linux 64
+
+mkdir -p release/mprocs-$VERSION-linux64
 
 TARGET_CC=x86_64-linux-musl-gcc \
 RUSTFLAGS="-C linker=x86_64-linux-musl-gcc" \
 cargo build --release --target=x86_64-unknown-linux-musl
 
 cp target/x86_64-unknown-linux-musl/release/mprocs \
-  release/mprocs-$VERSION-linux64
+  release/mprocs-$VERSION-linux64/mprocs
 
-upx-head --brute release/mprocs-$VERSION-linux64
+upx-head --brute release/mprocs-$VERSION-linux64/mprocs
+
+tar -czvf release/mprocs-$VERSION-linux64.tar.gz \
+  -C release/mprocs-$VERSION-linux64 \
+  mprocs
 
 # Macos
+
+mkdir -p release/mprocs-$VERSION-macos64
 
 cargo build --release --target=x86_64-apple-darwin
 
 cp target/x86_64-apple-darwin/release/mprocs \
-  release/mprocs-$VERSION-macos64
+  release/mprocs-$VERSION-macos64/mprocs
 
-upx --brute release/mprocs-$VERSION-macos64
+upx --brute release/mprocs-$VERSION-macos64/mprocs
+
+tar -czvf release/mprocs-$VERSION-macos64.tar.gz \
+  -C release/mprocs-$VERSION-macos64 \
+  mprocs
