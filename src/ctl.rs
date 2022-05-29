67629bd792e457a335c0337fdd18faa08a3e9464
Pavel Volokitin <pavelvolokitin@gmail.com>
2022-05-29 20:38:26 +0700

    Print parsed yaml if ctl command deserialization failed

--- src/ctl.rs
+++ src/ctl.rs
@@ -1,10 +1,22 @@
+use serde_yaml::Value;
+
 use crate::{
   config::{Config, ServerConfig},
   event::AppEvent,
 };
 
 pub async fn run_ctl(ctl: &str, config: &Config) -> anyhow::Result<()> {
-  let event: AppEvent = serde_yaml::from_str(ctl)?;
+  let event: AppEvent = match serde_yaml::from_str(ctl) {
+    Ok(event) => event,
+    Err(err) => {
+      let val: Value = serde_yaml::from_str(ctl)?;
+      println!(
+        "Remote command parsed as:\n{}",
+        serde_yaml::to_string(&val)?
+      );
+      return Err(err.into());
+    }
+  };
 
   let socket = match &config.server {
     Some(ServerConfig::Tcp(addr)) => std::net::TcpStream::connect(addr)?,
