09d7ba4d8ed9253a6fc4cce78cdafcad4b7a8490
Pavel Volokitin <pavelvolokitin@gmail.com>
2022-06-28 23:22:27 +0700

    Add basic virterm tests

---
+++ tests/tests.lua
@@ -0,0 +1,36 @@
+function test(name, f)
+  print("TEST: " .. name)
+  f()
+end
+
+test("next proc", function()
+  local proc = vt.start('cargo r -- "echo one" "echo two" "echo three"')
+
+  local mark = "•"
+
+  proc:wait_text(mark .. "echo one")
+
+  proc:send_str("j")
+
+  proc:wait_text(mark .. "echo two")
+
+  proc:send_str("q")
+  proc:wait()
+end)
+
+test("next proc", function()
+  local proc = vt.start("cargo r -- nvim")
+
+  proc:wait_text("[No Name]")
+
+  proc:send_key("<C-a>")
+  proc:send_str("ihello!")
+  proc:send_key("<Esc>")
+  proc:send_str("i ")
+
+  proc:wait_text("hello !")
+
+  proc:send_key("<C-a>")
+  proc:send_str("q")
+  proc:wait()
+end)
