4603668d129a0b4893678e728bac7536a4b0ed96
Pavel Volokitin <pavelvolokitin@gmail.com>
2024-06-24 23:01:10 +0700

    Update windows build script

---
+++ scripts/build-windows-x86_64.cmd
@@ -0,0 +1,13 @@
+SET VERSION=0.7.0
+
+MKDIR release\mprocs-%VERSION%-windows-x86_64 || exit /b
+
+:: Windows x64
+
+cargo build --release || exit /b
+
+COPY target\release\mprocs.exe release\mprocs-%VERSION%-windows-x86_64\mprocs.exe || exit /b
+
+:: upx --brute release\mprocs-%VERSION%-windows-x86_64\mprocs.exe || exit /b
+
+tar.exe -a -c -f release\mprocs-%VERSION%-windows-x86_64.zip -C release\mprocs-%VERSION%-windows-x86_64 mprocs.exe
