596da6761ef3e8faabc425bc20bd517ca5516046
Pavel Volokitin <pavelvolokitin@gmail.com>
2025-04-20 20:46:15 +0200

    Fix build scripts for aarch64 and x86_64

--- scripts/build-linux-x86_64.sh
+++ scripts/build-linux-x86_64.sh
@@ -7,13 +7,13 @@ mkdir -p release
 podman run -it --workdir /app \
   --platform linux/amd64 \
   --env ARCH=x86_64 \
-  -v $(pwd)/scripts:/app/scripts:Z \
-  -v $(pwd)/Cargo.lock:/app/Cargo.lock:Z \
-  -v $(pwd)/Cargo.toml:/app/Cargo.toml:Z \
-  -v $(pwd)/vendor:/app/vendor:Z \
-  -v $(pwd)/src:/app/src:Z \
-  -v $(pwd)/helpers:/app/helpers:Z \
-  -v $(pwd)/scripts:/app/scripts:Z \
-  -v $(pwd)/release:/app/release:Z \
+  -v $(pwd)/scripts:/app/scripts \
+  -v $(pwd)/Cargo.lock:/app/Cargo.lock \
+  -v $(pwd)/Cargo.toml:/app/Cargo.toml \
+  -v $(pwd)/vendor:/app/vendor \
+  -v $(pwd)/src:/app/src \
+  -v $(pwd)/helpers:/app/helpers \
+  -v $(pwd)/scripts:/app/scripts \
+  -v $(pwd)/release:/app/release \
   rust:1.78.0-alpine3.20 \
   scripts/_build-linux.sh
